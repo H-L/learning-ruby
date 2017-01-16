019b391069eca20c5b367c68622e3c1d30f15f0c
H-L <hadrien.lepoutre@gmail.com>
2017-01-16 19:51:53 +0100

    [Routes] Creating routes for articles and chapters

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root to: 'articles#index'
   root to: 'chapters#index'
+  get 'chapters', to: 'chapters#index'
+  get 'articles', to: 'articles#index'
 end
